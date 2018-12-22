9f98fcd41ec1eb2172c0763b85c917fdcbc9aff3
Roberto Pintos López <roberto@secture.com>
2018-12-22 14:21:24 +0100

    Added operation manager tests.

--- src/test/task/operation/operation-manager.test.ts
+++ src/test/task/operation/operation-manager.test.ts
@@ -3,6 +3,7 @@ import { ITaskFlowPartEndArgs } from '../../../task/flow/task-flow-part-event-ar
 import { OperationManager } from '../../../task/operation/operation-manager';
 import { ITest } from '../../ITest';
 import { LogMessageTaskFlowPart } from '../../logic/flow/log-message-task-flow-part';
+import { SampleLog } from '../../logic/sample-log';
 
 const operationManagerTestEventAlias = 'test-alias';
 
@@ -10,6 +11,64 @@ export class OperationManagerTests implements ITest {
     public performTests(): void {
         describe('Operation Manager Tests', () => {
             this.itMustBeInitializable();
+            this.itMustBeAbleToRaiseAnEventListeners();
+            this.itMustBeAbleToRaiseMultipleEventListeners();
+        });
+    }
+
+    private itMustBeAbleToRaiseAnEventListeners() {
+        it('mustBeAbleToRaiseAnEventListeners', () => {
+            const eventEmitter = new EventEmitter();
+            const operationManager = new OperationManager<ITaskFlowPartEndArgs<LogMessageTaskFlowPart>>(
+                operationManagerTestEventAlias,
+                eventEmitter,
+            );
+            const log = new SampleLog();
+            operationManager.subscribe(
+                'sample-alias',
+                (eventArgs) => log.logMessage(eventArgs.part.message),
+            );
+            eventEmitter.emit(
+                operationManagerTestEventAlias,
+                {
+                    aliases: ['sample-alias'],
+                    part: new LogMessageTaskFlowPart(
+                        'sample-task-alias',
+                        'sample-message-text',
+                    ),
+                } as ITaskFlowPartEndArgs<LogMessageTaskFlowPart>,
+            );
+            expect(log.getMessages()).toEqual(['sample-message-text']);
+        });
+    }
+
+    private itMustBeAbleToRaiseMultipleEventListeners() {
+        it('mustBeAbleToRaiseMultipleEventListeners', () => {
+            const eventEmitter = new EventEmitter();
+            const operationManager = new OperationManager<ITaskFlowPartEndArgs<LogMessageTaskFlowPart>>(
+                operationManagerTestEventAlias,
+                eventEmitter,
+            );
+            const log = new SampleLog();
+            operationManager.subscribe(
+                'sample-alias',
+                (eventArgs) => log.logMessage(eventArgs.part.message),
+            );
+            operationManager.subscribe(
+                'sample-alias-2',
+                (eventArgs) => log.logMessage(eventArgs.part.message),
+            );
+            eventEmitter.emit(
+                operationManagerTestEventAlias,
+                {
+                    aliases: null,
+                    part: new LogMessageTaskFlowPart(
+                        'sample-task-alias',
+                        'sample-message-text',
+                    ),
+                } as ITaskFlowPartEndArgs<LogMessageTaskFlowPart>,
+            );
+            expect(log.getMessages()).toEqual(['sample-message-text', 'sample-message-text']);
         });
     }
 
